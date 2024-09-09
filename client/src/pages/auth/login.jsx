290187c5b0f58efe2313e0bd20ae1581d862dde7
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-09 05:39:51 +0300

    Refactor routing and add authentication UI components

---
+++ client/src/pages/auth/login.jsx
@@ -0,0 +1,11 @@
+
+
+
+function AuthLogin() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
+export default AuthLogin;
---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,11 @@
+
+
+
+function AuthRegister() {
+    return (
+      <div>
+        <h1>Register</h1>
+      </div>
+    );
+  }
+  export default AuthRegister;
