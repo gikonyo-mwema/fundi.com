21ba69e435e1667dd1e61aa8fbc6de2d09039c34
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-15 22:34:43 +0300

    Refactor import statement in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; 
 
+import store from "./store/store";  
+import React from 'react';
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
