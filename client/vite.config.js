6a533e6f94090236cca75058be0a70588069a5b3
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-15 15:23:26 +0300

    Refactor import statement in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Add this line
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
+    },
+  },
 });
 
