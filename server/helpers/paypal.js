d4e522152657cb3acfb26e3fe7a62c96f7995935
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-16 17:32:20 +0300

    Refactor PayPal configuration in paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,8 +1,18 @@
-const paypal = require("paypal-rest-sdk");
+const dotenv = require('dotenv');
+const paypal = require('paypal-rest-sdk');
+
+dotenv.config();
+
+const { PAYPAL_MODE, PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = process.env;
+
+if (!PAYPAL_MODE || !PAYPAL_CLIENT_ID || !PAYPAL_CLIENT_SECRET) {
+    throw new Error('Missing PayPal configuration in environment variables');
+}
 
 paypal.configure({
- mode: "",
-client_id: "",
-client_secret: "",});
+    mode: PAYPAL_MODE, // 'sandbox' or 'live'
+    client_id: PAYPAL_CLIENT_ID,
+    client_secret: PAYPAL_CLIENT_SECRET,
+});
 
 module.exports = paypal;
