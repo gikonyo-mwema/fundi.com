b13806b98f6a3095f8d1663759d6f31dd175220e
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-22 13:26:53 +0300

    Add imageUploader and paymentGateway helpers

---
+++ fundi-ecommerce/server/helpers/imageUploader.js
@@ -0,0 +1,36 @@
+const cloudinary = require('cloudinary').v2;
+
+const configureCloudinary = () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
+};
+
+module.exports = { configureCloudinary };
+
+
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
