ba52f2e06920cddbd2588d660d63098d6b84e8dd
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-22 13:23:53 +0300

    Refactor AdminView components and add new components for OrderDetailsView and ProductTile

---
+++ fundi-ecommerce/client/src/components/AdminView/order-details.jsx
@@ -0,0 +1,165 @@
+// Import necessary hooks and components
+import { useState } from "react";
+import CommonForm from "../common/form";
+import { DialogContent } from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+import { Badge } from "../ui/badge";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllOrdersForAdmin,
+  getOrderDetailsForAdmin,
+  updateOrderStatus,
+} from "@/store/admin/order-slice";
+import { useToast } from "../ui/use-toast";
+
+// Initial form data state
+const initialFormData = {
+  status: "",
+};
+
+function AdminOrderDetailsView({ orderDetails }) {
+  // State for form data
+  const [formData, setFormData] = useState(initialFormData);
+  // Get user data from Redux store
+  const { user } = useSelector((state) => state.auth);
+  // Initialize dispatch function
+  const dispatch = useDispatch();
+  // Initialize toast function
+  const { toast } = useToast();
+
+  // Log order details for debugging
+  console.log(orderDetails, "orderDetailsorderDetails");
+
+  // Handle the update status form submission
+  function handleUpdateStatus(event) {
+    event.preventDefault();
+    const { status } = formData;
+
+    // Dispatch the updateOrderStatus action
+    dispatch(
+      updateOrderStatus({ id: orderDetails?._id, orderStatus: status })
+    ).then((data) => {
+      if (data?.payload?.success) {
+        // Refresh order details and all orders for admin
+        dispatch(getOrderDetailsForAdmin(orderDetails?._id));
+        dispatch(getAllOrdersForAdmin());
+        // Reset form data
+        setFormData(initialFormData);
+        // Show success toast message
+        toast({
+          title: data?.payload?.message,
+        });
+      }
+    });
+  }
+
+  return (
+    <DialogContent className="sm:max-w-[600px]">
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          {/* Display order ID */}
+          <div className="flex mt-6 items-center justify-between">
+            <p className="font-medium">Order ID</p>
+            <Label>{orderDetails?._id}</Label>
+          </div>
+          {/* Display order date */}
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Order Date</p>
+            <Label>{orderDetails?.orderDate.split("T")[0]}</Label>
+          </div>
+          {/* Display order price */}
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Order Price</p>
+            <Label>${orderDetails?.totalAmount}</Label>
+          </div>
+          {/* Display payment method */}
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Payment method</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+          {/* Display payment status */}
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Payment Status</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
+          </div>
+          {/* Display order status with badge */}
+          <div className="flex mt-2 items-center justify-between">
+            <p className="font-medium">Order Status</p>
+            <Label>
+              <Badge
+                className={`py-1 px-3 ${
+                  orderDetails?.orderStatus === "confirmed"
+                    ? "bg-green-500"
+                    : orderDetails?.orderStatus === "rejected"
+                    ? "bg-red-600"
+                    : "bg-black"
+                }`}
+              >
+                {orderDetails?.orderStatus}
+              </Badge>
+            </Label>
+          </div>
+        </div>
+        <Separator />
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="font-medium">Order Details</div>
+            <ul className="grid gap-3">
+              {/* Display cart items */}
+              {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
+                ? orderDetails?.cartItems.map((item) => (
+                    <li className="flex items-center justify-between">
+                      <span>Title: {item.title}</span>
+                      <span>Quantity: {item.quantity}</span>
+                      <span>Price: ${item.price}</span>
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <div className="font-medium">Shipping Info</div>
+            <div className="grid gap-0.5 text-muted-foreground">
+              {/* Display shipping info */}
+              <span>{user.userName}</span>
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {/* Form to update order status */}
+          <CommonForm
+            formControls={[
+              {
+                label: "Order Status",
+                name: "status",
+                componentType: "select",
+                options: [
+                  { id: "pending", label: "Pending" },
+                  { id: "inProcess", label: "In Process" },
+                  { id: "inShipping", label: "In Shipping" },
+                  { id: "delivered", label: "Delivered" },
+                  { id: "rejected", label: "Rejected" },
+                ],
+              },
+            ]}
+            formData={formData}
+            setFormData={setFormData}
+            buttonText={"Update Order Status"}
+            onSubmit={handleUpdateStatus}
+          />
+        </div>
+      </div>
+    </DialogContent>
+  );
+}
+
+export default AdminOrderDetailsView;
