5a312447200d223c4877fa1a2eefd103a973d80d
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-09-22 13:17:39 +0300

    Refactor index.js, ProductCard.jsx, CartContext.jsx, Cart.jsx, CheckAuth.jsx, Login.jsx, and Signup.jsx

---
+++ fundi-ecommerce/client/src/components/common/CheckAuth.jsx
@@ -0,0 +1,68 @@
+import { Navigate, useLocation } from "react-router-dom";
+
+function CheckAuth({ isAuthenticated, user, children }) {
+  const location = useLocation();
+
+  console.log(location.pathname, isAuthenticated);
+
+  // Redirect to login if not authenticated and trying to access the home page
+  if (location.pathname === "/") {
+    if (!isAuthenticated) {
+      return <Navigate to="/auth/login" />;
+    } else {
+      // Redirect to appropriate dashboard based on user role
+      if (user?.role === "admin") {
+        return <Navigate to="/admin/dashboard" />;
+      } else {
+        return <Navigate to="/shop/home" />;
+      }
+    }
+  }
+
+  // Redirect to login if not authenticated and trying to access protected routes
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  // Redirect authenticated users away from login or register pages
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/register"))
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
+
+  // Prevent non-admin users from accessing admin routes
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  // Prevent admin users from accessing shop routes
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  // Render children if no redirection is needed
+  return <>{children}</>;
+}
+
+export default CheckAuth;
